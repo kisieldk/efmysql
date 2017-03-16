6f27eebc1f3ed6a5e3e3abf6a826560cee907b73
Damian Kisielewski <namian.Kisielewski@dfc.local>
2017-03-16 15:26:38 +0100

    init

---
+++ MySql/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySql
+{
+	public class Model
+	{
+		public class LibraryContext : DbContext
+		{
+			public LibraryContext() : base("LibraryContext") { }
+			public DbSet<Book> Books { get; set; }
+
+		}
+
+		public class Book
+		{
+			[Key]
+			public int Id { get; set; }
+			public string Title { get; set; }
+		}
+	}
+}
